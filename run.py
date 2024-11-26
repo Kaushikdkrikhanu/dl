7c415bd16f3433521e976f0b78edd93725b01425
Amar Maruf <amarmaruf@Amars-MacBook-Pro.local>
2024-11-25 17:04:51 -0800

    fix

--- run.py
+++ run.py
@@ -40,7 +40,7 @@ except LookupError:
 
 # Configure logging
 logging.basicConfig(
-    level='INFO',
+    level=logging.INFO,
     format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
