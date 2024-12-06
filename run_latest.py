cecfc87b87af3b40d7a524d1796e7acbad239c14
Amar Maruf <amarmaruf@Amars-MacBook-Pro.local>
2024-12-06 02:01:32 -0800

    fix

--- run_latest.py
+++ run_latest.py
@@ -485,8 +485,6 @@ class AdvancedModel(nn.Module):
                 num_return_sequences=num_return_sequences,
                 pad_token_id=self.tokenizer.pad_token_id,
                 eos_token_id=self.tokenizer.eos_token_id,
-                no_repeat_ngram_size=3,  # Prevent repetition
-                repetition_penalty=1.2  # Penalize repetition
             )
 
             return outputs
